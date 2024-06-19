a80cc68898d7aecf73360e7ee7a7374b1863ef46
Shr13050 <Shryl13050@gmail.com>
2024-06-19 20:30:45 +0530

    minor updations

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,5 +3,5 @@
 import mongoose from "mongoose"
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://shryl13050:KPQmVS8pVd9Qhe8r@cluster0.ugwcrny.mongodb.net/tomato').then(()=> console.log("db connected"));
+    await mongoose.connect(Mongouri).then(()=> console.log("db connected"));
 }
